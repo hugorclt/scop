0bc0489cdc10d0fd36a47d4385d885725bf90a06
hugorclt <hugorecolet@gmail.com>
2023-01-30 19:50:12 +0100

    [update]windows creation working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents,
+};
+use vulkano::device::physical::PhysicalDeviceType;
+use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo};
+use vulkano::image::view::ImageView;
+use vulkano::image::{ImageAccess, SwapchainImage};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::pipeline::graphics::viewport::Viewport;
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass};
+use vulkano::swapchain::{
+    self, AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError,
+    SwapchainPresentInfo,
+};
+use vulkano::sync::{self, FlushError, GpuFuture};
+use vulkano::{Version, VulkanLibrary};
+
+use vulkano_win::VkSurfaceBuild;
+
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+use std::sync::Arc;
+
+fn main()
+{
+    let instance = {
+        let library = VulkanLibrary::new()
+            .expect("Expected to create a library for the instance");
+        let extensions = vulkano_win::required_extensions(&library);
+
+        Instance::new(
+            library,
+            InstanceCreateInfo {
+                enabled_extensions: extensions,
+                enumerate_portability: true, // required for MoltenVK on macos
+                max_api_version: Some(Version::V1_1),
+                ..Default::default()
+            },
+        )
+        .expect("Expected to return an instance")
+    };
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .expect("Expected to create a surface");
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+
+    let (physical_device, queue_family_index) = instance
+        .enumerate_physical_devices()
+        .unwrap()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .filter_map(|p| {
+            p.queue_family_properties()
+                .iter()
+                .enumerate()
+                .position(|(i, q)| {
+                    q.queue_flags.graphics 
+                        && p.surface_support(i as u32, &surface).unwrap_or(false)
+                })
+                .map(|i| (p, i as u32))
+        })
+        .min_by_key(|(p, _)| {
+            match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
+            }
+        })
+        .expect("Expected to find the best physical device on your computer");
+    
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo { 
+                enabled_extensions: device_extensions,
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        ).expect("Expected to create a device (software) from the physical device we found");
+
+        let queue = queues.next().unwrap();
 }
