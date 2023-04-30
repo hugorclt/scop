5bf24a8e45bf9a04e98b533f87797e10174b5b4e
hugorclt <hugorecolet@gmail.com>
2023-04-30 21:15:09 +0200

    [UPDATE]added accessor to push data in the Scene vector

--- srcs/Scene/Scene.cpp
+++ srcs/Scene/Scene.cpp
@@ -3,3 +3,27 @@
 void	Scene::setName(std::string name) {
 	_name = name;
 }
+
+void	Scene::addVertex(Vec3 *vertex) {
+	_vertex.push_back(vertex);
+}
+
+void	Scene::addTexture(Vec3 *texture) {
+	_textures.push_back(texture);
+}
+
+void	Scene::addNormal(Vec3 *normal) {
+	_normals.push_back(normal);
+}
+
+void	Scene::addMaterial(Material *material) {
+	_materials.push_back(material);
+}
+
+void	Scene::addFace(Face *face) {
+	_faces.push_back(face);
+}
+
+void	Scene::shading(bool shading) {
+	_shading = shading;
+}
--- srcs/Scene/Scene.hpp
+++ srcs/Scene/Scene.hpp
@@ -7,13 +7,19 @@
 class Scene {
 	private:
 		std::string 			_name;
-		std::vector<Vec3>		_vertex;
-		std::vector<Vec3>		_textures;
-		std::vector<Vec3>		_normals;
-		std::vector<Material>	_materials;
-		std::vector<Face>		_faces;
-		bool					shading;
+		std::vector<Vec3*>		_vertex;
+		std::vector<Vec3*>		_textures;
+		std::vector<Vec3*>		_normals;
+		std::vector<Material*>	_materials;
+		std::vector<Face*>		_faces;
+		bool					_shading;
 
 	public:
 		void	setName(std::string name);
+		void	addVertex(Vec3 *vertex);
+		void	addTexture(Vec3 *texture);
+		void	addNormal(Vec3 *texture);
+		void	addMaterial(Material *material);
+		void	addFace(Face *face);
+		void	shading(bool shading);
 };
